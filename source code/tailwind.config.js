51816ccc2124a57651c4b872e5c65dc363881616
Faded <anonfaded@pm.me>
2024-03-05 21:57:07 +0500

    autobash.script

---
+++ source code/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      screens: {
+        'sm': '412px', // Small screen / phone
+      },
+    },
+  },
+  plugins: [],
+}
